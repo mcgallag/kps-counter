983cfb945f5a864a365fe2f7f316fb5200fce003
Mike Gallagher <mcgallag@gmail.com>
2019-02-04 10:43:47 -0500

    Initial commit for CLI bridging program

---
+++ kps-counter/DOSBoxMemoryReader.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using Memory;
+
+namespace kps_counter
+{
+    /// <summary>
+    /// Interface for reading memory values from DOSBox
+    /// </summary>
+    class WingCommanderMemoryReader
+    {
+        private Mem m;
+        private int pid;
+
+        /// <summary>
+        /// true if successfully hooked to the DOSBox process
+        /// </summary>
+        public bool OpenProc { get; private set; } = false;
+
+        /// <summary>
+        /// base memory address for DOSBox RAM
+        /// </summary>
+        private const string memoryBase = "0x01D3A1A0";
+        /// <summary>
+        /// memory offset for player callsign
+        /// </summary>
+        private const string callSignOffset = "0x1E040";
+        /// <summary>
+        /// memory offset for player name
+        /// </summary>
+        private const string playerNameOffset = "0x1E032";
+        /// <summary>
+        /// memory offset for player's sortie count
+        /// </summary>
+        private const string sortiesOffset = "0x1E052";
+        /// <summary>
+        /// memory offset for player's board kills
+        /// </summary>
+        private const string boardKillsOffset = "0x1E054";
+        /// <summary>
+        /// memory offset for player's in-mission kills
+        /// </summary>
+        private const string currentKillsOffset = "0x1E402";
+        /// <summary>
+        /// memory offset for player's rank value
+        /// </summary>
+        private const string rankOffset = "0x1E050";
+        /// <summary>
+        /// memory offset for player's ship kps
+        /// </summary>
+        private const string setKpsOffset = "0x1FFE7";
+        /// <summary>
+        /// memory offset for player's remaining afterburner fuel
+        /// </summary>
+        private const string remainingFuelOffset = "0x2045C";
+
+        /// <summary>
+        /// used to properly calcuate number of player kills, set when we enter Halcyon's debriefing and cleared at mission start
+        /// </summary>
+        public bool DebriefMode = false;
+
+        /// <summary>
+        /// instantiates a new MemoryReader
+        /// </summary>
+        public WingCommanderMemoryReader()
+        {
+            m = new Mem();
+
+            GetPID();
+        }
+
+        /// <summary>
+        /// returns the process id of DOSBox if available
+        /// </summary>
+        /// <returns></returns>
+        public int GetPID()
+        {
+            pid = m.getProcIDFromName("DOSBox");
+            return pid;
+        }
+
+        /// <summary>
+        /// attaches the memory reader interface to DOSBox throws DOSBoxMemoryException on error
+        /// </summary>
+        public void hook()
+        {
+            // refresh PID just to be safe
+            GetPID();
+            if (pid == 0) throw new DOSBoxMemoryException("getProcIDFromName returned 0. Are you sure DOSBox.exe is running?");
+            OpenProc = m.OpenProcess(pid);
+            if (OpenProc == false) throw new DOSBoxMemoryException("OpenProcess returned false. Are administrative priveleges acquried?");
+        }
+
+        /// <summary>
+        /// fetches set kps value from DOSBox memory
+        /// </summary>
+        /// <returns>set kps</returns>
+        public int GetSetKPS()
+        {
+            if (OpenProc)
+            {
+                string addr = memoryBase + "," + setKpsOffset;
+                return m.readByte(addr) * 10;
+            }
+            throw new DOSBoxMemoryException("Must hook to DOSBox process before reading memory.");
+        }
+
+        /// <summary>
+        /// fetches player name from DOSBox memory
+        /// </summary>
+        /// <returns>player name</returns>
+        public string GetPlayerName()
+        {
+            if (OpenProc)
+            {
+                string addr = memoryBase + "," + playerNameOffset;
+                return m.readString(addr, length: 14);
+            }
+            throw new DOSBoxMemoryException("Must hook to DOSBox process before reading memory.");
+        }
+
+        /// <summary>
+        /// fetches player callsign from DOSBox memory
+        /// </summary>
+        /// <returns>player callsign</returns>
+        public string GetPlayerCallsign()
+        {
+            if (OpenProc)
+            {
+                string addr = memoryBase + "," + callSignOffset;
+                return m.readString(addr, length: 14);
+            }
+            throw new DOSBoxMemoryException("Must hook to DOSBox process before reading memory.");
+        }
+
+        /// <summary>
+        /// fetches player sortie count from DOSBox memory
+        /// </summary>
+        /// <returns>player sortie count</returns>
+        public int GetSorties()
+        {
+            if (OpenProc)
+            {
+                string addr = memoryBase + "," + sortiesOffset;
+                return m.read2Byte(addr);
+            }
+            throw new DOSBoxMemoryException("Must hook to DOSBox process before reading memory.");
+        }
+
+        /// <summary>
+        /// fetches player killboard kills from DOSBox memory
+        /// </summary>
+        /// <returns>player killboard kill count</returns>
+        public int GetBoardKills()
+        {
+            if (OpenProc)
+            {
+                string addr = memoryBase + "," + boardKillsOffset;
+                return m.read2Byte(addr);
+            }
+            throw new DOSBoxMemoryException("Must hook to DOSBox process before reading memory.");
+        }
+
+        /// <summary>
+        /// fetches player in-mission kills from DOSBox memory
+        /// </summary>
+        /// <returns>player in-mission kill count</returns>
+        public int GetCurrentKills()
+        {
+            if (OpenProc)
+            {
+                string addr = memoryBase + "," + currentKillsOffset;
+                return m.read2Byte(addr);
+            }
+            throw new DOSBoxMemoryException("Must hook to DOSBox process before reading memory.");
+        }
+
+        /// <summary>
+        /// fetches player rank value from DOSBox memory
+        /// </summary>
+        /// <returns>player rank value</returns>
+        public int GetRank()
+        {
+            if (OpenProc)
+            {
+                string addr = memoryBase + "," + rankOffset;
+                return m.read2Byte(addr);
+            }
+            throw new DOSBoxMemoryException("Must hook to DOSBox process before reading memory.");
+        }
+    }
+
+
+    [Serializable]
+    public class DOSBoxMemoryException : Exception
+    {
+        public DOSBoxMemoryException() { }
+        public DOSBoxMemoryException(string message) : base(message) { }
+        public DOSBoxMemoryException(string message, Exception inner) : base(message, inner) { }
+        protected DOSBoxMemoryException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
--- kps-counter/Program.cs
+++ kps-counter/Program.cs
@@ -1,15 +1,112 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO.Ports;
 
 namespace kps_counter
 {
+    /// <summary>
+    /// Bridges connection between DOSBox memory for Wing Commander 1 and Arduino 7-segment 4-digit display
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// serial connection to the Arduino
+        /// </summary>
+        static SerialPort port;
+
         static void Main(string[] args)
         {
+            int baud = 0;
+            string name;
+            Console.WriteLine("Welcome, enter parameters to begin");
+            Console.WriteLine();
+            Console.WriteLine("Available Ports:");
+            if (SerialPort.GetPortNames().Count() > 0)
+            {
+                foreach (string portname in SerialPort.GetPortNames())
+                {
+                    Console.WriteLine("  " + portname);
+                }
+            } else
+            {
+                Console.WriteLine("No ports available! Press any key to exit.");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.Write("Port Name [Press Enter for COM3 default]: ");
+            name = Console.ReadLine();
+            if (name == "") name = "COM3";
+
+            Console.WriteLine();
+            Console.Write("Baud Rate [Press Enter for 9600 default]: ");
+            try
+            {
+                baud = int.Parse(Console.ReadLine());
+            } catch
+            {
+                // default to 9600, as set in arduino sketch
+                baud = 9600;
+            }
+
+            try
+            {
+                Console.WriteLine();
+                Console.WriteLine("Opening serial connection to Arduino...");
+                port = new SerialPort(name, baud);
+                port.DataReceived += new SerialDataReceivedEventHandler(Port_DataReceived);
+                port.Open();
+                Console.WriteLine("Serial connection with Arduino started.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception caught while opening serial connection! Aborting...");
+                Console.WriteLine(e);
+                return;
+            }
+
+
+            WingCommanderMemoryReader reader = new WingCommanderMemoryReader();
+
+            try
+            {
+                Console.WriteLine();
+                Console.WriteLine("Attaching to DOSBox.exe process...");
+                reader.hook();
+                Console.WriteLine("Successfully attached!");
+            } catch (DOSBoxMemoryException e)
+            {
+                Console.WriteLine("Exception caught while attaching to DOSBox! Aborting...");
+                Console.WriteLine(e);
+                return;
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Now communicating between DOSBox and Arduino.");
+            Console.WriteLine("Press Ctrl-C to exit program");
+            Console.WriteLine("");
+
+            // loop until user aborts program
+            while (true)
+            {
+                // fetch from memory and write to the serial port
+                int kps = reader.GetSetKPS();
+                port.WriteLine(kps.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Event handler for Arduino serial input
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // HACK - pause for a bit to let the serial connection catch up
+            for (int i = 0; i < (10000 * port.BytesToRead) / port.BaudRate; i++) ;
+            // output to display
+            Console.WriteLine(port.ReadExisting());
+            Console.WriteLine("");
         }
     }
 }
